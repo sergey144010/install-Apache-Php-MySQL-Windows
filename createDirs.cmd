fab994c40081b428887c87535ee000c7223d6ec8
sergey144010 <sergey144010@mail.ru>
2015-11-14 12:45:08 +0800

    change

--- createDirs.cmd
+++ createDirs.cmd
@@ -1,26 +1,51 @@
 @echo off
 cd c:\
 
+goto comment1
+-----------------
+:: Внимательно Устанавливаем SERVER_DIR
+:: Правильно - SERVER_DIR=c:\serverTEST
+:: Не правильно - SERVER_DIR = c:\serverTEST
+-----------------
+:comment1
+
 set SERVER_DIR=c:\serverTEST
 
+if exist "%SERVER_DIR%" (
+    goto end
+) else (
+    goto begin
+)
+
+:end
+echo Already exists %SERVER_DIR%. Set the other value variable SERVER_DIR.
+pause
+exit
+
+:begin
 set WEB_DIR=%SERVER_DIR%\web
 set DOMAINS_DIR=%SERVER_DIR%\domains
 
-echo Create Dir - %SERVER_DIR%
 mkdir %SERVER_DIR%
-echo Create Dir - %WEB_DIR%
 mkdir %WEB_DIR%
-echo Create Dir - %DOMAINS_DIR%
 mkdir %DOMAINS_DIR%
-
-echo Create SubDir
+echo Create Dir - %SERVER_DIR%
+echo Create Dir - %WEB_DIR%
+echo Create Dir - %DOMAINS_DIR%
 
 mkdir %WEB_DIR%\apache
 mkdir %WEB_DIR%\php
 mkdir %WEB_DIR%\mysql
+echo Create Dir - %WEB_DIR%\apache
+echo Create Dir - %WEB_DIR%\php
+echo Create Dir - %WEB_DIR%\mysql
 
 mkdir %DOMAINS_DIR%\localhost
 mkdir %DOMAINS_DIR%\localhost\public_html
 mkdir %DOMAINS_DIR%\localhost\logs
+echo Create Dir - %DOMAINS_DIR%\localhost
+echo Create Dir - %DOMAINS_DIR%\localhost\public_html
+echo Create Dir - %DOMAINS_DIR%\localhost\logs
 
 pause
+exit
